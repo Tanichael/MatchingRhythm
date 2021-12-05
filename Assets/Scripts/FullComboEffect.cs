c4bf69f50bee51537199fd4d48800facdb333357
谷川斗真 <toma92toop@gmail.com>
2021-12-05 21:31:54 +0900

    UserDataクラスとFullComboEffectの実装

---
+++ Assets/Scripts/FullComboEffect.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FullComboEffect : MonoBehaviour
+{
+    [SerializeField] private float m_Speed = 1.0f;
+    [SerializeField] private Text m_Text;
+    [SerializeField] private AudioSource m_FullComboAudio;
+
+    private readonly float ms_WaitTime = 0.8f;
+
+    public AudioSource FullComboAudio
+    {
+        get
+        {
+            return m_FullComboAudio;
+        }
+    }
+
+    private float m_Time = 0f;
+
+    private void Update()
+    {
+        RectTransform rectTransform = this.gameObject.transform as RectTransform;
+        if(rectTransform != null)
+        {
+            rectTransform.localScale = GetScale();
+        }
+        m_Text.color = GetAlpha(m_Text.color);
+        if(GetScale().x > 10f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private Vector3 GetScale()
+    {
+        Vector3 scale = new Vector3(1f, 1f, 0);
+        m_Time += Time.deltaTime * m_Speed;
+        if (m_Time < ms_WaitTime) return scale;
+        scale = new Vector3(1f + (m_Time - ms_WaitTime) * (m_Time - ms_WaitTime), 1f + (m_Time - ms_WaitTime) * (m_Time - ms_WaitTime), 1f);
+
+        return scale;
+    }
+
+    private Color GetAlpha(Color color)
+    {
+        m_Time += Time.deltaTime * m_Speed;
+        if (m_Time < ms_WaitTime) return color;
+        color.a = 1.0f - (m_Time - ms_WaitTime) * (m_Time - ms_WaitTime) / 10f;
+
+        return color;
+    }
+}
---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : Singleton<UserData>
+{
+    /// <summary>
+    /// 曲のidを受け取ってハイスコアを返す関数
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public float GetHighMoteIndex(int id)
+    {
+        float highMoteIndex = PlayerPrefs.GetFloat(GetHighMoteIndexKeyFromId(id));
+        return highMoteIndex;
+    }
+
+    public void SetHighMoteIndex(int id, float moteIndex)
+    {
+        PlayerPrefs.SetFloat(GetHighMoteIndexKeyFromId(id), moteIndex);
+        PlayerPrefs.Save();
+    }
+
+    /// <summary>
+    /// 曲のidを受け取って最大コンボ数を返す関数
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public int GetHighCombo(int id)
+    {
+        int highCombo = PlayerPrefs.GetInt(GetHighMoteIndexKeyFromId(id));
+        return highCombo;
+    }
+
+    public void SetHighCombo(int id, int maxCombo)
+    {
+        PlayerPrefs.SetInt(GetHighComboKeyFromId(id), maxCombo);
+        PlayerPrefs.Save();
+    }
+
+    private string GetHighMoteIndexKeyFromId(int id)
+    {
+        string key;
+        key = id.ToString() + "_high_mote_index";
+        return key;
+    }
+
+    private string GetHighComboKeyFromId(int id)
+    {
+        string key;
+        key = id.ToString() + "_high_combo";
+        return key;
+    }
+
+}
