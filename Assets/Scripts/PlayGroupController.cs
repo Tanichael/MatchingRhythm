68d3e92da713e4d3e83f674e47c225f4db0ad2ef
谷川斗真 <toma92toop@gmail.com>
2021-12-06 02:16:28 +0900

    実装

--- Assets/Scripts/PlayGroupController.cs
+++ Assets/Scripts/PlayGroupController.cs
@@ -17,45 +17,25 @@ public class PlayGroupController : MonoBehaviour
 
     private readonly float ms_HorizontalRange = 320f;
     private readonly float ms_VerticalRange = 380f;
-    private readonly float ms_CheckDist = 0.1f;
+    private readonly float ms_CheckDist = 0.0001f;
 
-    public void SetUp(MusicData musicData)
+    private bool m_IsBack = false;
+    private bool m_IsPlay = false;
+    private int m_Id;
+
+    private void OnEnable()
     {
-        this.gameObject.SetActive(false);
-        this.gameObject.SetActive(true);
-
-        //各種テキストの設定
-        string title = musicData.Title.Replace(" ", "");
-        float highMoteIndex = UserData.Instance.GetHighMoteIndex(musicData.Id);
-        int highCombo = UserData.Instance.GetHighCombo(musicData.Id);
-        string highMoteIndexText = highMoteIndex.ToString() + "％";
-        string highComboText = highCombo.ToString();
-
-        m_TitleText.text = title;
-        m_HighMoteIndexText.text = highMoteIndexText;
-        m_HighComboText.text = highComboText;
-
-        //位置、サイズの設定
         RectTransform rectTransform = this.gameObject.transform as RectTransform;
-        int id = musicData.Id;
-        if (rectTransform != null)
-        {
-            rectTransform.anchorMin = new Vector2(0.5f, 1f);
-            rectTransform.anchorMax = new Vector2(0.5f, 1f);
-
-            RectTransform baseRectTransform = m_MusicElement.transform as RectTransform;
-
-            int right = id % 3;
-            int down = id / 3;
-
-            rectTransform.anchoredPosition = baseRectTransform.anchoredPosition + new Vector2(ms_HorizontalRange * right, -ms_VerticalRange * down);
-        }
 
         m_PlayButton.interactable = false;
         m_BackButton.interactable = false;
 
+        m_IsBack = false;
+        m_IsPlay = false;
+
         this.UpdateAsObservable()
-            .Subscribe(_ =>
+            .Where(_ => m_IsBack == false)
+            .Subscribe(__ =>
             {
                 Vector2 finalAnchorMin = new Vector2(0f, 0f);
                 Vector2 finalAnchorMax = new Vector2(1f, 1f);
@@ -77,39 +57,98 @@ public class PlayGroupController : MonoBehaviour
 
             });
 
-        m_PlayButton.onClick
-            .AsObservable()
-            .Subscribe(_ =>
+
+        this.UpdateAsObservable()
+            .Where(_ => m_IsBack == true)
+            .Subscribe(__ =>
             {
+                m_PlayButton.interactable = false;
+                m_BackButton.interactable = false;
+                m_IsBack = false;
+
+                this.gameObject.SetActive(false);
+                SceneLoader.Instance.TransparentUnCover();
+
+                //Vector2 finalAnchorMin = new Vector2(0.5f, 1f);
+                //Vector2 finalAnchorMax = new Vector2(0.5f, 1f);
+
+                //RectTransform baseRectTransform = m_MusicElement.transform as RectTransform;
+                //int right = m_Id % 3;
+                //int down = m_Id / 3;
+                //Vector2 finalPosition = baseRectTransform.anchoredPosition + new Vector2(ms_HorizontalRange * right, -ms_VerticalRange * down);
+
+                //if ((Vector2.Distance(rectTransform.anchorMin, finalAnchorMin) > ms_CheckDist) || (Vector2.Distance(rectTransform.anchorMax, finalAnchorMax) > ms_CheckDist) || (Vector2.Distance(rectTransform.anchoredPosition, finalPosition) > ms_CheckDist))
+                //{
+                //    rectTransform.anchorMin = Vector2.Lerp(rectTransform.anchorMin, finalAnchorMin, 0.3f);
+                //    rectTransform.anchorMax = Vector2.Lerp(rectTransform.anchorMax, finalAnchorMax, 0.3f);
+                //    rectTransform.anchoredPosition = Vector2.Lerp(rectTransform.anchoredPosition, finalPosition, 0.3f);
+                //}
+
+                //if ((Vector2.Distance(rectTransform.anchorMin, finalAnchorMin) <= ms_CheckDist) && (Vector2.Distance(rectTransform.anchorMax, finalAnchorMax) <= ms_CheckDist))
+                //{
+                //    this.gameObject.SetActive(false);
+                //    m_IsBack = false;
+                //    SceneLoader.Instance.TransparentUnCover();
+                //}
+
+            });
+
+    }
+
+    public void SetUp(MusicData musicData)
+    {
+        //位置、サイズの設定
+        RectTransform rectTransform = this.gameObject.transform as RectTransform;
+        int id = musicData.Id;
+        m_Id = id;
+        if (rectTransform != null)
+        {
+            rectTransform.anchorMin = new Vector2(0.5f, 1f);
+            rectTransform.anchorMax = new Vector2(0.5f, 1f);
+
+            RectTransform baseRectTransform = m_MusicElement.transform as RectTransform;
+
+            int right = id % 3;
+            int down = id / 3;
+
+            rectTransform.anchoredPosition = baseRectTransform.anchoredPosition + new Vector2(ms_HorizontalRange * right, -ms_VerticalRange * down);
+        }
+        m_IsBack = false;
+
+        this.gameObject.SetActive(false);
+        this.gameObject.SetActive(true);
+
+        //各種テキストの設定
+        string title = musicData.Title.Replace(" ", "");
+        float highMoteIndex = UserData.Instance.GetHighMoteIndex(musicData.Id);
+        int highCombo = UserData.Instance.GetHighCombo(musicData.Id);
+        string highMoteIndexText = highMoteIndex.ToString() + "％";
+        string highComboText = highCombo.ToString();
+
+        m_TitleText.text = title;
+        m_HighMoteIndexText.text = highMoteIndexText;
+        m_HighComboText.text = highComboText;
+
+        m_PlayButton.onClick
+           .AsObservable()
+           .Where(_ => m_IsPlay == false)
+           .Subscribe(__ =>
+           {
+                m_IsPlay = true;
                 //拡大みたいなの挟んだ方がよさそう、とりあえず暫定
                 DataManager.Instance.MusicData = musicData;
                 //シーン切り替え 読み込みどこ？
                 //シーン切り替えた後ロードして準備ができたら音楽を流す、って感じで良さそう
                 //つまりここではとりあえずシーンを切り替える
                 SceneLoader.Instance.GoSceneAsync("GameScene").Forget();
-
-            });
+           });
 
         m_BackButton.onClick
             .AsObservable()
             .Subscribe(_ =>
             {
-                this.gameObject.SetActive(false);
-                if (rectTransform != null)
-                {
-                    rectTransform.anchorMin = new Vector2(0.5f, 1f);
-                    rectTransform.anchorMax = new Vector2(0.5f, 1f);
-
-                    RectTransform baseRectTransform = m_MusicElement.transform as RectTransform;
-
-                    int right = id % 3;
-                    int down = id / 3;
-
-                    rectTransform.anchoredPosition = baseRectTransform.anchoredPosition + new Vector2(ms_HorizontalRange * right, -ms_VerticalRange * down);
-                }
-
+                SceneLoader.Instance.TransparentCover();
+                m_IsBack = true;
             });
-
-
     }
 }
